f13fc66c732147f601061c7da592beeae3530917
li-yinan <biline@163.com>
2012-02-26 21:55:13 +0800

    move code from obj into sprite

--- javascripts/v0.4lib/obj.js
+++ javascripts/v0.4lib/obj.js
@@ -3,27 +3,6 @@ Obj = function(images){
 	var _this = this;
 	this.speed = 100;
 	this.img = images.sprite;
-	var _cxt=document.getElementById("canvas").getContext("2d");
-	this.moveTo = function(){
-		var sx = _this.anchorX;
-		var sy = _this.anchorY;
-		var dx = _this.destX;
-		var dy = _this.destY;
-		//if near destination then stop move
-		if(Math.abs(sx-dx)<3&&Math.abs(sy-dy)<3){
-			//ticker.clearEvent(moveptr);
-			return;
-		}
-		var tick = ticker.getTick();
-		var unit = _this.speed/Math.sqrt((dx-sx)*(dx-sx)+(dy-sy)*(dy-sy))*tick/1000;
-		_this.anchorX = unit*(dx-sx)+sx;
-		_this.anchorY = unit*(dy-sy)+sy;
-		console.log("moveto override");
-	};
-	this.draw = function(){
-		_cxt.clearRect(0,0,800,600);
-		_cxt.drawImage(_this.img,_this.anchorX-_this.collisionR/2,_this.anchorY-_this.collisionR/2,_this.collisionR,_this.collisionR);
-	}
 };
 
 function main1(){
@@ -31,8 +10,7 @@ function main1(){
 	function start(images){
 		obj = new Obj(images);
 		ticker = new Ticker("",1000/60);//frequency
-		moveptr = ticker.addEvent(obj.moveTo);
-		ticker.addEvent(obj.draw);
+		ticker.addEvent(obj.frameCtrl);
 		ticker.start();
 	}
 
