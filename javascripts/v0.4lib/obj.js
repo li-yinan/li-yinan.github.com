e163b8f61a47d37ad6241a43dc22cccea0ebdadd
li-yinan <biline@163.com>
2012-02-26 19:05:02 +0800

    add eventManager

--- javascripts/v0.4lib/obj.js
+++ javascripts/v0.4lib/obj.js
@@ -1,31 +1,42 @@
-Obj = function(){
+Obj = function(images){
 	Sprite.call(this);
+	this.img = images.sprite;
+	var _cxt=document.getElementById("canvas").getContext("2d");
 	this.moveTo = function(destX,destY){
 		console.log("moveto override");
 	};
 	this.draw = function(){
-		var c=document.getElementById("canvas");
-		var cxt=c.getContext("2d");
-		var imgresource = {
-			sprite:"resource/spriteimg/images.jpg"
-		}
-		new ImgLoader(imgresource,function(images){
-			cxt.drawImage(images.sprite,0,0,200,200);
-		});
+		_cxt.clearRect(0,0,800,600);
+		_cxt.drawImage(this.img,this.anchorX,this.anchorY,200,200);
 	}
 };
 
 function main1(){
-	var obj = new Obj();
-	obj.moveTo(1,2);
-	obj.draw();
-	//var _res = new Resource(snakeNum);
-
-	//var _ticker = _res.getTicker().getPhysics();
-	//_ticker.addEvent(snake0.userCtrl);
-	//_ticker.addEvent(snake1.freewalk);
-	//_ticker.addEvent(snake2.freewalk);
-	//_ticker.start();
+	function start(images){
+		obj = new Obj(images);
+		obj.draw();
+	}
+	var imgresource = {
+		sprite:"resource/spriteimg/images.jpg"
+	};
+	new ImgLoader(imgresource,start);
+	var evtMgr = new EventManager();
+	evtMgr.addEvent(38,function(){
+		obj.anchorY-=10;
+		obj.draw();
+	});
+	evtMgr.addEvent(39,function(){
+		obj.anchorX+=10;
+		obj.draw();
+	});
+	evtMgr.addEvent(40,function(){
+		obj.anchorY+=10;
+		obj.draw();
+	});
+	evtMgr.addEvent(37,function(){
+		obj.anchorX-=10;
+		obj.draw();
+	});
 };
 
 window.onload = main1;
