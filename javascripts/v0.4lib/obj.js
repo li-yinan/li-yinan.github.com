301e7e4f0ac8cdcd1417d42d3094e08179d12b6a
li-yinan <biline@163.com>
2012-02-27 21:11:22 +0800

    add sprite saperater

--- javascripts/v0.4lib/obj.js
+++ javascripts/v0.4lib/obj.js
@@ -4,22 +4,24 @@ Obj = function(images){
 	this.anchorX = 400;
 	this.anchorY = 300;
 	this.speed = 200;
-	this.collisionR = 50;
-	this.scale = 4;
+	this.collisionR = 20;
+	this.scale = 1;
 	this.duration = 300;
-	this.img = images.sprite4;
-	this.spriteList.push([0,0,226,226]);
-	this.spriteList.push([227,0,226,226]);
+	this.img = images.sprite5;
+	//this.spriteList.push([0,0,120,120]);
+	//this.spriteList.push([227,0,226,226]);
 	//this.img = images.sprite2;
 	//this.spriteList.push([22,7,95,95]);
 	//this.spriteList.push([141,7,95,95]);
 	//this.spriteList.push([261,7,95,95]);
+	
 };
 
 function main1(){
 
 	function start(images){
 		obj = new Obj(images);
+		obj.calRegionCoord(4,4);
 		ticker = new Ticker("",60);//frequency
 		ticker.addEvent(obj.frameCtrl);
 		ticker.start();
@@ -29,7 +31,8 @@ function main1(){
 		sprite1:"resource/spriteimg/images.jpg",
 		sprite2:"resource/spriteimg/sprite.jpg",
 		sprite3:"resource/spriteimg/02.png",
-		sprite4:"resource/spriteimg/03.png"
+		sprite4:"resource/spriteimg/03.png",
+		sprite5:"resource/spriteimg/012-Lancer04.png"
 	};
 
 	new ImgLoader(imgresource,start);
@@ -37,15 +40,23 @@ function main1(){
 	var evtMgr = new EventManager();
 
 	evtMgr.addKeyEvent(38,function(){
+		obj.direction = 3;
+		obj.eventSource = "key";
 		obj.setDest(400,200);
 	});
 	evtMgr.addKeyEvent(39,function(){
+		obj.direction = 2;
+		obj.eventSource = "key";
 		obj.setDest(500,300);
 	});
 	evtMgr.addKeyEvent(40,function(){
+		obj.direction = 0;
+		obj.eventSource = "key";
 		obj.setDest(400,400);
 	});
 	evtMgr.addKeyEvent(37,function(){
+		obj.direction = 1;
+		obj.eventSource = "key";
 		obj.setDest(300,300);
 	});
 
--- javascripts/v0.4lib/sprite.js
+++ javascripts/v0.4lib/sprite.js
@@ -1,6 +1,10 @@
 Sprite = function(){
 	// image of sprite
 	this.img = undefined;
+	// sprite direction
+	this.direction = 0;
+	// controll event source
+	this.eventSource = "";
 	// region of img
 	this.spriteList = [];
 	//current sprite
@@ -108,24 +112,63 @@ Sprite = function(){
 	 *
 	 * @return 
 	 */
-	this.setSpriteRegion = function(i){
-		if(i>=_this.spriteList.length){
+	this.setSpriteRegion = function(direction,i){
+		if(direction){
+			_this.direction = direction;
+		}
+		if(i>=_this.spriteList[_this.direction].length){
 			i = 0;
 		}
-		_this.imgRegionLeft = _this.spriteList[i][0];
-		_this.imgRegionRight = _this.spriteList[i][1];
-		_this.imgRegionWidth = _this.spriteList[i][2];
-		_this.imgRegionHeight = _this.spriteList[i][3];
+		_this.imgRegionLeft = _this.spriteList[_this.direction][i][0];
+		_this.imgRegionRight = _this.spriteList[_this.direction][i][1];
+		_this.imgRegionWidth = _this.spriteList[_this.direction][i][2];
+		_this.imgRegionHeight = _this.spriteList[_this.direction][i][3];
 		_this.currentSprite = i;
 	};
 
+	/**
+	 * @brief calculate the direction,and set the sprite correct sprite img
+	 *
+	 * @return 
+	 */
 	this.switchSpriteRegion = function(){
+		var sx = _this.anchorX;
+		var sy = _this.anchorY;
+		var dx = _this.destX;
+		var dy = _this.destY;
+		var direction = undefined;
+		//decide direction
+		// if event source is keyboard,direction = input arrow
+		// else calulate the direction 
+		if(_this.eventSource!="key"){
+			var movex = Math.abs(dx-sx)>Math.abs(dy-sy)?true:false;
+			if(!_this.moving){
+				//sprite is stoped
+				direction = 0;
+			}else{
+				//sprite move on x axis
+				if(movex){
+					if(dx>sx){
+						direction = 2;//move to right
+					}else{
+						direction = 1;//move to left
+					}
+				}else{
+				//sprite move on y axis
+					if(dy>sy){
+						direction = 0;//move to right
+					}else{
+						direction = 3;//move to left
+					}
+				}
+			}
+		}
+		//decide direction end
 		var freq = ticker.getFreq();
 		var times = parseInt(_this.duration/1000*freq);
 		if(ticker.getCounter()%times==0){
-			_this.setSpriteRegion(_this.currentSprite+1);
+			_this.setSpriteRegion(direction, _this.currentSprite+1);
 		}
-
 	};
 
 	/**
@@ -136,17 +179,42 @@ Sprite = function(){
 	 */
 	this.draw = function(){
 		_this.cxt.clearRect(0,0,_this.canvas.width,_this.canvas.height);
+		//draw collison circle
+		_this.cxt.beginPath();
+		_this.cxt.arc(_this.anchorX,_this.anchorY,_this.collisionR,0,Math.PI*2,true);
+		_this.cxt.stroke();
+		//draw collison circle end
 		_this.cxt.drawImage(
 			_this.img,
 			_this.imgRegionLeft,
 			_this.imgRegionRight,
 			_this.imgRegionWidth,
 			_this.imgRegionHeight,
-			_this.anchorX-_this.collisionR/2*_this.scale,
-			_this.anchorY-_this.collisionR/2*_this.scale,
-			_this.collisionR*_this.scale,
-			_this.collisionR*_this.scale
+			_this.anchorX-_this.collisionR*_this.scale,
+			_this.anchorY-_this.imgRegionHeight*2*_this.collisionR/_this.imgRegionWidth*_this.scale,
+			_this.collisionR*2*_this.scale,
+			_this.imgRegionHeight*2*_this.collisionR/_this.imgRegionWidth*_this.scale
 		);
+	};
+
+	/**
+	 * @brief seperate img into pieces of sprite
+	 *
+	 * @param numX x axis sprite numbers
+	 * @param numY y axis sprite numbers
+	 *
+	 * @return 
+	 */
+	this.calRegionCoord = function(numX,numY){
+		var regionWidth = _this.img.width/numX;
+		var regionHeight = _this.img.height/numY;
+		for(var j=0;j<numY;j++){
+			//define direction array
+			_this.spriteList[j] = [];
+			for(var i=0;i<numX;i++){
+				_this.spriteList[j].push([i*regionWidth,j*regionHeight,regionWidth,regionHeight]);
+			}
+		}
 	};
 
 	/**
