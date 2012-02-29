5006b2812d006c76a456546837dfd566237e8776
li-yinan <biline@163.com>
2012-02-29 21:18:06 +0800

    add clock self adaptation

--- javascripts/v0.4lib/ticker.js
+++ javascripts/v0.4lib/ticker.js
@@ -1,4 +1,4 @@
-Ticker = function(resource,freq){
+Ticker = function(freq){
 	var _evtList = [];
 	var _tickptr;
 	var _freq = freq;
@@ -38,6 +38,29 @@ Ticker = function(resource,freq){
 
 	this.start = function(){
 		_active = true;
+		_tickptr = setInterval(_this.doEvent, 1000/_freq);
+		// frame self adaptation
+		var lastcnt = 0;
+		var score = document.getElementById("scorer");
+		//add a 1min timer to count ticks
+		setInterval(function(){
+			var frame = _counter-lastcnt;
+			//console.log("ticks in 1min is"+(_counter-lastcnt));
+			score.innerHTML = frame;
+			if(_freq-frame>3){
+				_this.restart(frame);
+			}else{
+				_this.restart(_freq+1);
+			}
+			lastcnt = _counter;
+		}, 1000);
+		// frame self adaptation end
+	};
+
+	this.restart = function(freq){
+		if(freq>60){freq=60;}
+		_freq = freq;
+		clearInterval(_tickptr);
 		_tickptr = setInterval(_this.doEvent, 1000/_freq);
 	};
 
