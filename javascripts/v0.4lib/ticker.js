5f5b465cff9530301dc21e109c7477680d0ba10d
li-yinan <biline@163.com>
2012-03-01 21:13:28 +0800

    add sprite rotate

--- javascripts/v0.4lib/ticker.js
+++ javascripts/v0.4lib/ticker.js
@@ -1,4 +1,12 @@
-Ticker = function(freq){
+/**
+ * @brief 
+ *
+ * @param freq frequency
+ * @param auto whether auto change freq
+ *
+ * @return 
+ */
+Ticker = function(freq,auto){
 	if(!freq){
 		freq = 60;
 	}
@@ -35,7 +43,7 @@ Ticker = function(freq){
 		_counter++;
 		for(var i=0;i<_evtList.length;i++){
 			if(_evtList[i]){
-				setTimeout(_evtList[i],0);
+				_evtList[i]();
 			}
 		}
 	};
@@ -43,26 +51,29 @@ Ticker = function(freq){
 	this.start = function(){
 		_active = true;
 		_tickptr = setInterval(_this.doEvent, 1000/_freq);
-		// frame self adaptation
-		var lastcnt = 0;
-		var score = document.getElementById("scorer");
-		//add a 1min timer to count ticks
-		_frametimer = setInterval(function(){
-			var frame = _counter-lastcnt;
-			//console.log("ticks in 1min is"+(_counter-lastcnt));
-			score.innerHTML = frame;
-			if(_freq-frame>3){
-				_this.restart(frame);
-			}else{
-				_this.restart(_freq+1);
-			}
-			lastcnt = _counter;
-		}, 1000);
-		// frame self adaptation end
+		if(auto){
+			// frame self adaptation
+			var lastcnt = 0;
+			var score = document.getElementById("scorer");
+			//add a 1min timer to count ticks
+			_frametimer = setInterval(function(){
+				var frame = _counter-lastcnt;
+				//console.log("ticks in 1min is"+(_counter-lastcnt));
+				score.innerHTML = frame;
+				if(_freq-frame>3){
+					_this.restart(frame);
+				}else{
+					_this.restart(_freq+1);
+				}
+				lastcnt = _counter;
+			}, 1000);
+			// frame self adaptation end
+		}
 	};
 
 	this.restart = function(freq){
 		if(freq>60){freq=60;}
+		if(freq<30){freq=30;}
 		_freq = freq;
 		clearInterval(_tickptr);
 		_tickptr = setInterval(_this.doEvent, 1000/_freq);
