72f3d3c0a77db54d316c8e5e0f719b25870a2aa1
li-yinan <biline@163.com>
2012-03-10 19:59:52 +0800

    add calculate effect radian

--- javascripts/v0.4lib/fteffect.js
+++ javascripts/v0.4lib/fteffect.js
@@ -2,7 +2,7 @@ FtEffect = function(images,callback,unit1,unit2){
 	Effect.call(this);
 	var _this = this;
 	this.img = images.effect1;
-	this.radian = Math.PI/180*135;
+	this.radianFix = Math.PI/180*135;
 	//this.img = images.sprite1;
 	
 	//call sperate img into sprite function
